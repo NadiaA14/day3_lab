2f19248b82173b96d776846347c8450d497b7343
Nadia Al-Murshedy <nadia@Nadias-MacBook-Air.local>
2022-05-04 15:53:46 +0100

    first commit

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -20,3 +20,24 @@ def divide(number_1, number_2):
 def length_of_string(string):
     return len(string)
 
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(number):
+    if number == 1:
+        return "January"
+    if number == 3:
+        return "March"
+    if number == 9:
+        return "September"
+
+def number_to_short_month_name(number):
+    if number == 1:
+        return "Jan"
+    if number == 4:
+        return "Apr"
+    if number == 8:
+        return "Oct"
